7ad349f47adc31e60c4f6e35d786d326d23d5158
wildpow <element1028@gmail.com>
2020-11-27 23:26:46 -0800

    refactor: nav scaffolding

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
@@ -6,7 +8,18 @@ export default function Layout({ children }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav>Nav goes here</nav>
+      <header>
+        <Link href="/">Home</Link>
+        <nav>
+          <Link href="/create">
+            <a>Create</a>
+          </Link>
+          <Link href="/roadmap">
+            <a>Roadmap</a>
+          </Link>
+          <a href="#">Repo</a>
+        </nav>
+      </header>
       <main>{children}</main>
       <footer>Made with Love by Wildpow (FOOTER)</footer>
     </div>
