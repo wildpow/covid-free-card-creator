2a1d18db54727349a10dace4e5b363c453925d04
wildpow <element1028@gmail.com>
2020-11-27 22:04:18 -0800

    chore: added custom colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "santa-red": "A30202",
+        "santa-yellow": "FFB90D",
+        "santa-brown": "764F26",
+      },
+    },
   },
   variants: {},
   plugins: [],
