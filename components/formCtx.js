202f4ac5792dee3f1d707aed6960c949a4810075
wildpow <element1028@gmail.com>
2020-11-29 01:43:22 -0800

    feat: scaffolding for output

--- components/formCtx.js
+++ components/formCtx.js
@@ -15,24 +15,9 @@ const initialState = {
 };
 const reducer = (state, action) => {
   return { ...state, [action.type]: action.payload };
-  // switch (action.type) {
-  //   case "to":
-  //     return {...state, to: action.payload}
-  //   case 'date':
-  //     return {...state, [action.type]: action.payload}
-  //   case: "message":
-  // }
 };
 export const FormProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  // const [to, setTo] = useState("Miss Pacman");
-  // const [message, setMessage] = useState(
-  //   "Even though I have not seen you since February you are in my thoughts"
-  // );
-  // const [from, setFrom] = useState("Mr. Ninja Tiger");
-  // const newDate = new Date();
-  // const [date, setDate] = useState(newDate.toISOString().split("T")[0]);
-
   return (
     <FormCtx.Provider value={{ state, dispatch }}>{children}</FormCtx.Provider>
   );
--- pages/output.js
+++ pages/output.js
@@ -1,13 +1,25 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout from "../components/layout";
+import { useForm } from "../components/formCtx";
 
 export default function OutPutCard() {
+  const { state } = useForm();
   return (
     <Layout>
       <Head>
         <title>Card Creator -- Output</title>
       </Head>
       <h2>Output of card</h2>
+      <div>
+        <h3>{`Dear ${state.to},`}</h3>
+        <div>{state.date}</div>
+        <p>{state.message}</p>
+        <span>{`Love, ${state.from}`}</span>
+        <Link href="/create">
+          <a>Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
