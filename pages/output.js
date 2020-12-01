1aec6170dcf2072e75d643c0e4ebe0b33c6dcbbc
wildpow <element1028@gmail.com>
2020-12-01 02:51:28 -0800

    style(output): basic structor and responsiveness

--- pages/output.js
+++ pages/output.js
@@ -35,8 +35,8 @@ export default function OutPutCard() {
           <div
             style={{
               ...state.selectedBorderStyle,
-              width: "50vh",
-              height: "50vh",
+              width: "100%",
+              height: "90vh",
               backgroundColor: state.bgColor,
               color: state.textColor,
             }}
@@ -45,19 +45,23 @@ export default function OutPutCard() {
             <div
               className={`${""} grid grid-cols-1 gap-10 p-6 relative h-full`}
             >
-              <h3>{`Dear ${state.to},`}</h3>
-              <div>{state.date}</div>
-              <p>{state.message}</p>
-              <span>{`Love, ${state.from}`}</span>
+              <div className="flex justify-between">
+                <h3>{`Dear ${state.to},`}</h3>
+                <div className="text-sm">{state.date}</div>
+              </div>
+              <p className="max-w-2xl mb-6 mr-1 md:text-xl lg:text-3xl lg:leading-10 lg:mb-12 xl:leading-relaxed sm:max-w-sm lg:max-w-2xl">
+                {state.message}
+              </p>
+              <span className="self-center font-bold">{`Love, ${state.from}`}</span>
             </div>
           </div>
         )}
         <Link href="/create">
           <a className="max-w-md mx-auto btn2">
             <div className="">Edit</div>
-            <div className="w-5 ml-4 text-white md:w-8 md:mb-1 ">
+            {/* <div className="w-5 ml-4 text-white md:w-8 md:mb-1 ">
               <Hand />
-            </div>
+            </div> */}
           </a>
         </Link>
       </article>
